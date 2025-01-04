10e4f319cbe22a7b54a7d8cd25baac0f02fc6278
sdprog771 <sdprog771@gmail.com>
2025-01-04 19:42:58 +0200

    Add files via upload

---
+++ src/main/java/com/example/connecttologicapp1/ConnectToLogicApp1Application.java
@@ -0,0 +1,13 @@
+package com.example.connecttologicapp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectToLogicApp1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConnectToLogicApp1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/connecttologicapp1/ConnectToLogicApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.connecttologicapp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectToLogicApp1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
